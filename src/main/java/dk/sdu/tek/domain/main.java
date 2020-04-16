f1b4c413e453e2db6866fc11181d30224af0c893
jaco5157 <jacob-dan@hotmail.com>
2020-04-16 17:44:18 +0200

    Test to see if methods work

--- src/main/java/dk/sdu/tek/domain/main.java
+++ src/main/java/dk/sdu/tek/domain/main.java
@@ -1,9 +1,8 @@
 package dk.sdu.tek.domain;
 
-import dk.sdu.tek.persistance.ObjectReader;
+import dk.sdu.tek.persistence.ObjectReader;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class main {
     //Test class
@@ -21,5 +20,32 @@ public class main {
         System.out.println("Print single admin object");
         System.out.println(adminList.get(0));
         System.out.println(adminList.get(2));
+
+
+        System.out.println("Create objects and print:");
+        Producer producer = new Producer("producer", "producerpass");
+        System.out.println(producer);
+
+        Production production = new Production("production", 1);
+        System.out.println(production);
+
+        Person person = new Person("person", 1, "person@person.dk");
+        System.out.println(person);
+
+        Credit credit = new Credit(person.getPersonID(), production.getProductionID(), "somerole");
+        System.out.println(credit);
+
+        System.out.println("\nWrite  objects to files and read again");
+        producer.write();
+        production.write();
+        person.write();
+        credit.write();
+
+        //System.out.println(ObjectReader.readObject(ObjectReader.Type.ADMIN));
+        System.out.println(ObjectReader.readObject(ObjectReader.Type.PRODUCER));
+        System.out.println(ObjectReader.readObject(ObjectReader.Type.PRODUCTION));
+        System.out.println(ObjectReader.readObject(ObjectReader.Type.PERSON));
+        System.out.println(ObjectReader.readObject(ObjectReader.Type.CREDIT));
+
     }
 }
