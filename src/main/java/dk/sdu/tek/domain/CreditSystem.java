617c8ea54222c468bac34f2572816bee676ea483
jaco5157 <jacob-dan@hotmail.com>
2020-04-17 10:38:11 +0200

    Add authenticate method to CreditSystem

--- src/main/java/dk/sdu/tek/domain/CreditSystem.java
+++ src/main/java/dk/sdu/tek/domain/CreditSystem.java
@@ -1,4 +1,34 @@
 package dk.sdu.tek.domain;
 
+import javafx.scene.control.TextField;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class CreditSystem {
+    private Visitor currentUser;
+
+    public boolean authenticate(TextField one, TextField two) {
+        String tempUsername = "";
+        String tempPassword = "";
+
+
+
+        if(tempUsername.trim().equals(one.getText()) && tempPassword.trim().equals(two.getText())) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public 
+
+    public void setCurrentUser (Visitor currentUser) {
+        this.currentUser = currentUser;
+    }
+
+    public Visitor getCurrentUser() {
+        return currentUser;
+    }
 }
