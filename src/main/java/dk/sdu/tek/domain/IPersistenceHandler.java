224de610b0c97a2a06a770f3d8b90547dac5d3b6
jaco5157 <jacob-dan@hotmail.com>
2020-05-05 15:25:27 +0200

    Create IPersistencehandler interface

---
+++ src/main/java/dk/sdu/tek/domain/IPersistenceHandler.java
@@ -0,0 +1,40 @@
+package dk.sdu.tek.domain;
+
+import org.bson.types.ObjectId;
+
+import java.util.ArrayList;
+
+public interface IPersistenceHandler {
+
+    public ArrayList<Admin> getAdmins();
+
+    public Admin getAdmin(int id);
+
+    public boolean createAdmin(Admin admin);
+
+    public ArrayList<Producer> getProducers();
+
+    public Producer getProducer(int id);
+
+    public boolean createProducer(Producer producer);
+
+    public ArrayList<Production> getProductions();
+
+    public Production getProduction(int id);
+
+    public boolean createProduction(Production production);
+
+    public ArrayList<Person> getPeople();
+
+    public Person getPerson(int id);
+
+    public boolean createPerson(Person person);
+
+    public ArrayList<Credit> getCredits();
+
+    public Credit getCredit(int id);
+
+    public boolean createCredit(Credit credit);
+
+    public boolean deleteCredit(int id);
+}
