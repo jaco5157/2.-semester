64463ffcc9d0bb58270031434a59a27301a44035
jaco5157 <jacob-dan@hotmail.com>
2020-05-05 15:18:41 +0200

    add mongodb

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module dk.sdu.tek {
     requires javafx.controls;
     requires javafx.fxml;
+    requires mongo.java.driver;
 
     opens dk.sdu.tek.presentation to javafx.fxml;
     exports dk.sdu.tek.presentation;
