24b2cf7d3782967d897927c64c143ed6fc8b1550
jaco5157 <jacob-dan@hotmail.com>
2020-05-08 13:07:17 +0200

    Create ObservableObject.java

---
+++ src/main/java/dk/sdu/tek/domain/ObservableObject.java
@@ -0,0 +1,11 @@
+package dk.sdu.tek.domain;
+
+public class ObservableObject {
+    private int id;
+    private String object;
+
+    public ObservableObject (int id, String object) {
+        this.id = id;
+        this.object = object;
+    }
+}
