4a245384d1c4a8c7239266d71e07a34ae61452f9
jaco5157 <jacob-dan@hotmail.com>
2020-05-09 15:59:10 +0200

    Make Menus singleton

--- src/main/java/dk/sdu/tek/presentation/AdminMenu.java
+++ src/main/java/dk/sdu/tek/presentation/AdminMenu.java
@@ -3,8 +3,17 @@ package dk.sdu.tek.presentation;
 import java.io.IOException;
 
 public class AdminMenu implements Menu {
+    public static AdminMenu menu;
+
     @Override
     public void show() throws IOException {
         App.setRoot("adminmenu");
+    }
+
+    public static AdminMenu getMenu() {
+        if (menu == null) {
+            menu = new AdminMenu();
+        }
+        return menu;
     }
 }
--- src/main/java/dk/sdu/tek/presentation/ProducerMenu.java
+++ src/main/java/dk/sdu/tek/presentation/ProducerMenu.java
@@ -1,13 +1,19 @@
 package dk.sdu.tek.presentation;
 
-import dk.sdu.tek.presentation.App;
-import dk.sdu.tek.presentation.Menu;
-
 import java.io.IOException;
 
 public class ProducerMenu implements Menu {
+    private static ProducerMenu menu;
+
     @Override
     public void show() throws IOException {
         App.setRoot("producermenu");
+    }
+
+    public static ProducerMenu getMenu() {
+        if (menu == null) {
+            menu = new ProducerMenu();
+        }
+        return menu;
     }
 }
