3586db5ad0105997f2c44ac2804c8a98387b7385
jaco5157 <jacob-dan@hotmail.com>
2020-05-13 15:09:45 +0200

    Implement deleteCredit

--- src/main/java/dk/sdu/tek/presentation/SecondaryController.java
+++ src/main/java/dk/sdu/tek/presentation/SecondaryController.java
@@ -18,6 +18,7 @@ import javafx.stage.Stage;
 
 public class SecondaryController implements Initializable {
 
+    //FXML for search page
     @FXML private ImageView logo;
     @FXML private ImageView exit;
     @FXML private TabPane mainTab;
@@ -34,7 +35,7 @@ public class SecondaryController implements Initializable {
     @FXML private Label creditLabel;
     @FXML private ListView<ObservableObject> creditList;
 
-    //TextFields
+    //FXML for creating new objects
     @FXML private TextField createProducerUsername;
     @FXML private TextField createProducerPassword;
     @FXML private TextField createProducerId;
@@ -53,7 +54,20 @@ public class SecondaryController implements Initializable {
     @FXML private TextField createCreditRole;
 
     @FXML private Label successLabel;
-    
+
+    //FXML for editing credits
+    @FXML private TextField editCreditSearch;
+    @FXML private ListView<ObservableObject> resultCreditList;
+    @FXML private TextField editCreditId;
+    @FXML private TextField editCreditRole;
+    @FXML private TextField editCreditPersonId;
+    @FXML private TextField editCreditName;
+    @FXML private TextField editCreditProdId;
+    @FXML private TextField editCreditProdName;
+    @FXML private Button deleteCredit;
+    @FXML private Button editCredit;
+    @FXML private Label editLabel;
+
     private Stage stage;
     private double x = 0, y = 0;
 
@@ -62,12 +76,10 @@ public class SecondaryController implements Initializable {
         App.setRoot("primary");
     }
 
-    @FXML
     public void exit() {
         System.exit(1);
     }
 
-    @FXML
     public void makeDraggable() {
         mainTab.setOnMousePressed(((mouseEvent) -> {
             x = mouseEvent.getSceneX();
@@ -87,34 +99,35 @@ public class SecondaryController implements Initializable {
         App.setImageForImageView(logo,"Danish_TV_2_logo.png");
         App.setImageForImageView(exit, "red-x-mark.png");
         setResultList();
+        setCreditResultList();
     }
 
     public void setResultList () {
         creditLabel.setVisible(false);
         creditList.setVisible(false);
         if (selectProducers.isSelected()) {
-            resultList.setItems(search(CreditSystem.getInstance().getProducers()));
+            resultList.setItems(search(CreditSystem.getInstance().getProducers(), searchField.getText().toLowerCase()));
             selectItem.setText(selectProducers.getText());
         } else if (selectPeople.isSelected()) {
             creditLabel.setVisible(true);
             creditList.setVisible(true);
-            resultList.setItems(search(CreditSystem.getInstance().getPeople()));
+            resultList.setItems(search(CreditSystem.getInstance().getPeople(), searchField.getText().toLowerCase()));
             selectItem.setText(selectPeople.getText());
         } else if (selectCredits.isSelected()) {
-            resultList.setItems(search(CreditSystem.getInstance().getCredits()));
+            resultList.setItems(search(CreditSystem.getInstance().getCredits(), searchField.getText().toLowerCase()));
             selectItem.setText(selectCredits.getText());
         } else {
             creditLabel.setVisible(true);
             creditList.setVisible(true);
-            resultList.setItems(search(CreditSystem.getInstance().getProductions()));
+            resultList.setItems(search(CreditSystem.getInstance().getProductions(), searchField.getText().toLowerCase()));
             selectItem.setText(selectProductions.getText());
         }
     }
 
-    public ObservableList<ObservableObject> search(ObservableList<ObservableObject> objects) {
+    public ObservableList<ObservableObject> search(ObservableList<ObservableObject> objects, String query) {
         ObservableList<ObservableObject> searchResults = FXCollections.observableArrayList();
         for (ObservableObject object : objects) {
-            if (object.getObject().toLowerCase().contains(searchField.getText().toLowerCase())) {
+            if (object.getObject().toLowerCase().contains(query)) {
                 searchResults.add(object);
             }
         }
@@ -131,6 +144,7 @@ public class SecondaryController implements Initializable {
         }
     }
 
+    //Create new objects
     public void createProducer() {
         printSuccess(CreditSystem.getInstance().createProducer(Integer.parseInt(createProducerId.getText()), createProducerUsername.getText(), createProducerPassword.getText()));
     }
@@ -155,12 +169,35 @@ public class SecondaryController implements Initializable {
         }
     }
 
-//    public void deleteCredit(ActionEvent event) {
-//        Production selectedProduction = resultList.getSelectionModel().getSelectedItem();
-//        Credit selectedCredit = productionCreditList.getSelectionModel().getSelectedItem();
-//        if ((resultList.getSelectionModel().getSelectedItem() == selectedProduction) && (productionCreditList.getSelectionModel().getSelectedItem() == selectedCredit))
-//            productionCreditList.getItems().remove(selectedCredit);
-//        else if(resultList.getSelectionModel().getSelectedItem() == selectedProduction)
-//            resultList.getItems().remove(selectedProduction);
-//    }
+    //Edit credits
+    public void setCreditResultList() {
+        if (CreditSystem.getInstance().getIsAdmin()) {
+            resultCreditList.setItems(search(PersistenceHandler.getInstance().getAdmin(CreditSystem.getInstance().getUserId()).getOwnedCredits(), searchField.getText().toLowerCase()));
+        } else {
+            resultCreditList.setItems(search(PersistenceHandler.getInstance().getProducer(CreditSystem.getInstance().getUserId()).getOwnedCredits(), searchField.getText().toLowerCase()));
+        }
+    }
+
+    public void getSelectedCredit () {
+        Credit selectedCredit = PersistenceHandler.getInstance().getCredit(resultCreditList.getSelectionModel().getSelectedItem().getId());
+        editCreditId.setText(String.valueOf(selectedCredit.getId()));
+        editCreditRole.setText(selectedCredit.getRole());
+        editCreditPersonId.setText(String.valueOf(selectedCredit.getPersonID()));
+        editCreditName.setText(PersistenceHandler.getInstance().getPerson(selectedCredit.getPersonID()).getName());
+        editCreditProdId.setText(String.valueOf(selectedCredit.getProductionID()));
+        editCreditProdName.setText(PersistenceHandler.getInstance().getProduction(selectedCredit.getProductionID()).getName());
+    }
+
+    public void deleteCredit() {
+        if(PersistenceHandler.getInstance().deleteCredit(resultCreditList.getSelectionModel().getSelectedItem().getId())) {
+            editLabel.setText("Succes! Kreditering er slettet");
+            setCreditResultList();
+        } else {
+            editLabel.setText("Noget gik galt");
+        }
+    }
+
+    public void editCredit() {
+
+    }
 }
